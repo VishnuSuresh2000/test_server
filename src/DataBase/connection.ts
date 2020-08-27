3269ffaec1d6f3c5612ad0bdbfa7b91736215343
Vishnu Suresh <vishnusuresh19012000@gmail.com>
2020-08-27 19:19:24 +0530

    add oneline

--- src/DataBase/connection.ts
+++ src/DataBase/connection.ts
@@ -4,7 +4,7 @@ const connectionStart = (): void => {
     var db: string = "test"
     var localhost: string = "mongodb://localhost:27017/"
     var mblab: string = "mongodb+srv://berutest:test@1234@test-4h08c.mongodb.net/"
-    connect(localhost+ db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    connect(mblab+ db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
         console.log("DB Connected")
     }).catch((error) => {
         console.log("error on DB conection", error)
