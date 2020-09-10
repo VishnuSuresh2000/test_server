68a935a00c346a1fb7e942e85aea73de57bb0942
Vishnu Suresh <vishnusuresh19012000@gmail.com>
2020-09-10 22:21:59 +0530

    only one db

--- src/DataBase/connection.ts
+++ src/DataBase/connection.ts
@@ -3,7 +3,6 @@ import { connect, set } from 'mongoose'
 const connectionStart = (): void => {
     var db: string = "test"
     var localhost: string = "mongodb://localhost:27017/"
-    var mblab: string = "mongodb+srv://berutest:test@1234@test-4h08c.mongodb.net/"
     connect(localhost + db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
         console.log("DB Connected")
     }).catch((error) => {
