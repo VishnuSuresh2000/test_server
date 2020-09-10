b2e90567d381ea2705f8389a1993b7ea58c33f0b
Vishnu Suresh <vishnusuresh19012000@gmail.com>
2020-09-10 22:21:30 +0530

    added new admin sdk

--- src/Micro_Server_Connections/firebaaseAdmin.ts
+++ src/Micro_Server_Connections/firebaaseAdmin.ts
@@ -3,7 +3,7 @@ var serviceAccount =require('../../service_acount_firebase.json')
 
 admin.initializeApp({
     credential:admin.credential.cert(serviceAccount),
-    databaseURL: "https://beru-36801.firebaseio.com"
+    databaseURL: "https://beru-6665e.firebaseio.com"
 })
 
 export default admin
