31fea3943aba234a7c7ce13ab283c2cdc2b609c4
Vishnu Suresh <vishnusuresh19012000@gmail.com>
2020-09-10 17:10:05 +0530

    added some changes

--- src/Route/AllRouteBeru.ts
+++ src/Route/AllRouteBeru.ts
@@ -1,4 +1,17 @@
-
-// export default function allRoutesBeru(app: Application) {
-   
-// }
+import route_category from '../Section/Category/route'
+import customer from "../Section/Profiles/Customer/route"
+import seller from "../Section/Profiles/Seller/route";
+import product from "../Section/Product/route"
+import farmer from "../Section/Profiles/Farmer/route";
+import { route } from "../Section/Cart/route";
+import { Application } from "express";
+import { router } from '../Section/Salles/route';
+export default function allRoutesBeru(app: Application) {
+    app.use('/customer', customer)
+    app.use('/category', route_category)
+    app.use('/seller', seller)
+    app.use('/product', product)
+    app.use('/farmer', farmer)
+    app.use('/salles', router)
+    app.use('/cart', route)
+}
--- src/index.ts
+++ src/index.ts
@@ -4,40 +4,29 @@ import express, { Request, Response } from 'express'
 import expressWs from 'express-ws'
 import dbconnection from './DataBase/connection'
 import checkIfAuthenticated from './MiddleWare/Auth/auth'
-// import allRoutesBeru from './Route/AllRouteBeru'
+import allRoutesBeru from './Route/AllRouteBeru'
 import syncWebSocket from './WebSocket/websocket'
-import route_category from './Section/Category/route'
-import customer from "./Section/Profiles/Customer/route"
-import seller from "./Section/Profiles/Seller/route";
-import product from "./Section/Product/route"
-import farmer from "./Section/Profiles/Farmer/route";
-import { route } from "./Section/Cart/route";
 
-import { router } from './Section/Salles/route';
+
 
 dbconnection()
-var wsApp = expressWs(express())
+var wsApp=expressWs(express())
 var app = wsApp.app
+app.listen(5000, () => {
+    console.log("http://localhost:5000")
+})
 
 app.use(cors({
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "authorization"],
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token","authorization"],
     credentials: true,
     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
-
+    
 }))
 app.use(bodyParser.json({
     limit: '10mb'
 }))
-// allRoutesBeru(app)
-
-app.use('/customer', customer)
-app.use('/category', route_category)
-app.use('/seller', seller)
-app.use('/product', product)
-app.use('/farmer', farmer)
-app.use('/salles', router)
-app.use('/cart', route)
+allRoutesBeru(app)
 
 // app.use(function(req:Request,res:Response,next:NextFunction){
 //     var temp = res.send
@@ -49,7 +38,7 @@ app.use('/cart', route)
 // })
 
 app.get('/', (_req: Request, res: Response) => {
-    res.locals.test = "test"
+    res.locals.test="test"
     res.send("Test Sections")
 })
 // app.get('/testPdf', (_req: Request, res: Response) => {
@@ -57,15 +46,12 @@ app.get('/', (_req: Request, res: Response) => {
 //     invoiceTest(res)
 // })
 
-app.get('/test', checkIfAuthenticated, (_req: Request, res: Response) => {
+app.get('/test',checkIfAuthenticated, (_req: Request, res: Response) => {
     res.send(`test section ${res.locals.isAdmin}`)
 })
 
 syncWebSocket(app)
 
-app.listen(5000, () => {
-    console.log("http://localhost:5000")
-})
 
 
 
